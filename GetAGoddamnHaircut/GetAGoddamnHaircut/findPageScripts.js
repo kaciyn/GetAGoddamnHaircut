9eaa6ba1e22c53f2d3a6050dd6b313dc4b21204f
Elliot\Kaci <kaciyanova@gmail.com>
2018-11-14 00:24:06 +0000

    working review page scripts, map is a bit wonky on windows brave

---
+++ GetAGoddamnHaircut/GetAGoddamnHaircut/findPageScripts.js
@@ -0,0 +1,26 @@
+﻿import wixData from 'wix-data';
+
+$w.onReady(function() {
+    $w("#map").onMessage((event) => {
+        $w("#placeName").value = event.data.split(";")[0];
+        $w("#placeLocation").value = event.data.split(";")[1];
+        $w("#placeID").value = event.data.split(";")[2];
+
+    });
+
+    $w("#reviewsDataset").onBeforeSave(() => {
+        $w("#reviewsDataset").setFieldValue('title', $w("#placeName").value);
+        $w("#reviewsDataset").setFieldValue('name', $w("#placeLocation").value);
+        $w("#reviewsDataset").setFieldValue('address', $w("#placeID").value);
+    });
+});
+
+export function btnAddPronouns_click(event) {
+
+    $w("#pronounsDataset").refresh;
+    $w("#pronounsDataset").refresh;
+
+    console.log("Done refreshing the dataset");
+
+    $w("#btnAddPronouns").label = "Added!";
+}
---
+++ GetAGoddamnHaircut/GetAGoddamnHaircut/placesSearch.js
@@ -0,0 +1,27 @@
+﻿function getPlaceId() {
+    var input = document.getElementById('pac-input');
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+
+    autocomplete.setFields(
+        ['address_components', 'geometry', 'icon', 'name']);
+
+    autocomplete.addListener('place_changed',
+        function() {
+            var place = autocomplete.getPlace();
+           
+
+            var address = '';
+            if (place.address_components) {
+                address = [
+                    (place.address_components[0] && place.address_components[0].short_name || ''),
+                    (place.address_components[1] && place.address_components[1].short_name || ''),
+                    (place.address_components[2] && place.address_components[2].short_name || '')
+                ].join(' ');
+            }
+
+            return place;
+        });
+
+
+}
